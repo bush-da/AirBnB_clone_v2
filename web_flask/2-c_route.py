8151bef835147d35a2da1fe38fb5a00bb71f23df
Daniel Tujuma <danielsevani12@gmail.com>
2024-08-01 16:56:31 -0400

    fix bug

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,25 +5,28 @@
     Routes:
         /: display “Hello HBNB!”
         /hbnb: display “HBNB”
-        /c/<text>: display “C ” followed by the value of the text variable 
+        /c/<text>: display “C ” followed by the value of the text variable
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
+    """ Display Hello HBNB! """
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hello():
+    """ Display HBNB """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """ Display text variable value after C """
     return f"C {text.replace('_', ' ')}"
 
 if __name__ == "__main__":
