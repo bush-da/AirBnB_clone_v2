ca6fc4e69280eb0d82d9a238259b2c83a9685a40
Daniel Tujuma <danielsevani12@gmail.com>
2024-08-01 15:45:03 -0400

    add comment

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ function that returns when request get """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
