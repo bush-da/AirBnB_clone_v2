a296124d9e7414c31f8aff0b61d6fc2bf596ee71
Daniel Tujuma <danielsevani12@gmail.com>
2024-08-09 06:07:18 -0400

    fix typo bug

--- models/place.py
+++ models/place.py
@@ -5,6 +5,7 @@ from os import getenv
 from models.base_model import BaseModel
 from models.base_model import Base
 from models.amenity import Amenity
+from models.review import Review
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
@@ -16,7 +17,26 @@ association_table = Table(
 
 
 class Place(BaseModel, Base):
-    """ A place to stay """
+    """Represents a Place for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table places.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store places.
+        city_id (sqlalchemy String): The place's city id.
+        user_id (sqlalchemy String): The place's user id.
+        name (sqlalchemy String): The name.
+        description (sqlalchemy String): The description.
+        number_rooms (sqlalchemy Integer): The number of rooms.
+        number_bathrooms (sqlalchemy Integer): The number of bathrooms.
+        max_guest (sqlalchemy Integer): The maximum number of guests.
+        price_by_night (sqlalchemy Integer): The price by night.
+        latitude (sqlalchemy Float): The place's latitude.
+        longitude (sqlalchemy Float): The place's longitude.
+        reviews (sqlalchemy relationship): The Place-Review relationship.
+        amenities (sqlalchemy relationship): The Place-Amenity relationship.
+        amenity_ids (list): An id list of all linked amenities.
+    """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
@@ -32,7 +52,7 @@ class Place(BaseModel, Base):
     amenities = relationship("Amenity", secondary='place_amenity', viewonly=False)
     amenity_ids = []
 
-    if (getenv("HBNB_MYSQL_DB") != "db"):
+    if (getenv("HBNB_TYPE_STORAGE") != "db"):
         @property
         def reviews(self):
             """ get all reviews """
