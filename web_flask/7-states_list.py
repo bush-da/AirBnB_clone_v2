f7cb39d0880353d35de1b2d5bd99f19bbc6775c9
Daniel Tujuma <danielsevani12@gmail.com>
2024-08-10 18:31:34 -0400

    document module

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" module start flask web app list states in html webpage """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
